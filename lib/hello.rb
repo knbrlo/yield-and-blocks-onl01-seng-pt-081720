796b9d805f3066f10cd3f337dafc89d7856baae3
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 00:39:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,11 @@
-def yielding
-  puts "the program is executing the code inside the method"
-  yield
-  puts "now we are back in the method"
+def hello_t(array)
+  i = 0
+
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
+# call your method here!
 
-yielding { puts "the method has yielded to the block!" }
